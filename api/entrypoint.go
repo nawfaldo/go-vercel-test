6f03517197e4249244c535540a27ecd55060f2ac
nawfaldo <nawfaldo.main@gmail.com>
2024-07-31 21:03:27 +0700

    vercel

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -12,15 +12,30 @@ import (
 
 var (
 	router *mux.Router
-	db     *sql.DB
 )
 
-func init() {
+type Server struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewServer(addr string, db *sql.DB) *Server {
+	return &Server{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *Server) init() {
+	s.db, _ = sql.Open("postgres", "postgres://default:Ut4uNix0wdRk@ep-polished-sea-a1efivnq.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require")
+
 	router = mux.NewRouter()
 
 	v1 := router.PathPrefix("/api/v1").Subrouter()
 
-	user.RegisterRoutes(v1, db)
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
+	userHandler.RegisterRoutes(v1)
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"https://go-react-api-web.vercel.app"},
@@ -31,8 +46,6 @@ func init() {
 
 	handler := c.Handler(router)
 	http.Handle("/", handler)
-
-	db, _ = sql.Open("postgres", "postgres://default:Ut4uNix0wdRk@ep-polished-sea-a1efivnq.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require")
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
--- service/user/routes.go
+++ service/user/routes.go
@@ -1,41 +1,47 @@
 package user
 
 import (
-	"database/sql"
 	"net/http"
+	"vercer/types"
 	"vercer/utils"
 
 	"github.com/gorilla/mux"
 )
 
-var db *sql.DB
+type Handler struct {
+	store types.UserStore
+}
 
-func RegisterRoutes(router *mux.Router, database *sql.DB) {
-	db = database
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{
+		store: store,
+	}
+}
 
+func (h *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/users", handleGetUsers).Methods("GET")
 	router.HandleFunc("/user", handleCreateUsers).Methods("POST")
 }
 
 func handleGetUsers(w http.ResponseWriter, r *http.Request) {
-	rows, _ := db.Query("SELECT name FROM users")
-	defer rows.Close()
+	// rows, _ := db.Query("SELECT name FROM users")
+	// defer rows.Close()
 
-	type User struct {
-		Name string `json:"name"`
-	}
+	// type User struct {
+	// 	Name string `json:"name"`
+	// }
 
-	var users []User
+	// var users []User
 
-	for rows.Next() {
-		var u User
+	// for rows.Next() {
+	// 	var u User
 
-		rows.Scan(&u.Name)
+	// 	rows.Scan(&u.Name)
 
-		users = append(users, u)
-	}
+	// 	users = append(users, u)
+	// }
 
-	utils.WriteJSON(w, http.StatusAccepted, users)
+	utils.WriteJSON(w, http.StatusAccepted, "hello")
 }
 
 func handleCreateUsers(w http.ResponseWriter, r *http.Request) {
