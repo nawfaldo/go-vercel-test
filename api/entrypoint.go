15f7115cad06d056cb02078baedba123460edaad
nawfaldo <nawfaldo.main@gmail.com>
2024-07-31 19:27:55 +0700

    vercel

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -7,19 +7,24 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func API(w http.ResponseWriter, r *http.Request) {
+var (
+	router *mux.Router
+)
+
+func init() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/hello", hello).Methods("GET")
-
-	router.ServeHTTP(w, r)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-
 	hello := map[string]string{
 		"msg": "hello",
 	}
 
 	utils.WriteJSON(w, http.StatusAccepted, hello)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router.ServeHTTP(w, r)
 }
